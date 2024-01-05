15e965c991bff9d26c8e26907b58b014258551d0
assis1983 <assis.ederjd@gmail.com>
2024-01-05 15:47:30 -0300

    add images and texts from the treatments session

--- src/components/Treatments/index.tsx
+++ src/components/Treatments/index.tsx
@@ -1,4 +1,13 @@
-import { StyleTreatments } from "./style";
+import {
+  Frase,
+  StyleDivImages,
+  StyleTreatments,
+  ContainerImages,
+  Images,
+} from "./style";
+import Body from "../../Image/service-01.jpg";
+import Face from "../../Image/service-02.jpg";
+import Nutrition from "../../Image/service-03.jpg";
 
 const Treatments = () => {
   return (
@@ -6,6 +15,50 @@ const Treatments = () => {
       <StyleTreatments>
         Conheça <span>nossos Tratamentos</span>
       </StyleTreatments>
+      <Frase>Tratramentos Corporais e Faciais</Frase>
+      <ContainerImages>
+        <StyleDivImages>
+          <Images src={Body} />
+          <div>
+            <h4>Estética Corporal</h4>
+          </div>
+          <div className="text">
+            <p>
+              Na nossa clínica, dedicamo-nos a realçar a estética corporal de
+              forma saudável e personalizada. Com abordagens inovadoras,
+              promovemos a beleza interior e exterior, priorizando o bem-estar e
+              a autoconfiança.
+            </p>
+          </div>
+        </StyleDivImages>
+        <StyleDivImages>
+          <Images src={Face} />
+          <div>
+            <h4>Estética Facial</h4>
+          </div>
+          <div className="text">
+            <p>
+              Transforme sua beleza com a expertise da nossa clínica de estética
+              facial. Oferecemos tratamentos personalizados que realçam sua
+              luminosidade natural, promovendo uma pele saudável e radiante.
+            </p>
+          </div>
+        </StyleDivImages>
+        <StyleDivImages>
+          <Images src={Nutrition} />
+          <div>
+            <h4>Emagrecimento</h4>
+          </div>
+          <div className="text">
+            <p>
+              Descubra o caminho para uma jornada de emagrecimento saudável em
+              nossa clínica especializada. Com abordagem personalizada,
+              oferecemos programas eficazes que visam não apenas a perda de
+              peso, mas também o bem-estar geral. 
+            </p>
+          </div>
+        </StyleDivImages>
+      </ContainerImages>
     </>
   );
 };
--- src/components/Treatments/style.ts
+++ src/components/Treatments/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyleTreatments = styled.h1`
+export const StyleTreatments = styled.div`
     text-align: center;
     font-size: 2.5rem;
     span {
@@ -8,6 +8,39 @@ export const StyleTreatments = styled.h1`
           color: #8b008b; 
         }
     
-        
+`;
 
+export const Frase = styled.p`
+  font-size: 1.2rem;
+  text-align: center;
+`;
+
+export const ContainerImages = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 20px;
+  h4 {
+    color: #8b008b;
+    margin-top: 20px;
+  }
+  p {
+    text-align: justify;
+  }
+  .text {
+    max-width: 320px;
+  }
+`;
+
+export const StyleDivImages = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Images = styled.img`
+  width: 20rem;
 `;
